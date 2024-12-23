f2faba214d9a16e7adaa588c4461ab50f18453ab
DavidBezberd <151457121+DavidBezberd@users.noreply.github.com>
2024-12-23 22:16:37 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Calculator
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Введіть перше число: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Введіть дію (+, -, *, /): "); 
+            char operation = char.Parse(Console.ReadLine().Trim());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine($"Результат: {result}");
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine($"Результат: {result}");
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine($"Результат: {result}");
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine($"Результат: {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Помилка: ділення на нуль");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Неправильна операція");
+                    break;
+            }
+        }
+    }
+}
